128b0c7081cfe0643df6ca2589b83cee60c8f78f
radgast <radgastt@gmail.com>
2018-11-17 20:28:53 +0200

    Implement JPA Visit Service, added loader data closes # 47

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -49,8 +49,4 @@ public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetSe
         return super.findById(id);
     }
 
-    @Override
-    public Vet findByLastName(String lastName) {
-        return null;
-    }
 }
