7c0cff00bab25561492f89a694326531df024e64
radgast <radgastt@gmail.com>
2018-12-01 15:20:26 +0200

    added jpa test for owner service closes #50

---
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,72 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositiries.OwnerRepository;
+import guru.springframework.sfgpetclinic.repositiries.PetRepository;
+import guru.springframework.sfgpetclinic.repositiries.PetTypeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerSDJpaServiceTest {
+
+    @Mock
+    OwnerRepository ownerRepository;
+
+    @Mock
+    PetRepository petRepository;
+
+    @Mock
+    PetTypeRepository petTypeRepository;
+
+    @InjectMocks
+    OwnerSDJpaService service;
+
+    String LAST_NAME = "Smith";
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByLastName() {
+        Owner returnOwner = Owner.builder().id(1L).lastName(LAST_NAME).build();
+
+        when(ownerRepository.findByLastName(any())).thenReturn(returnOwner);
+
+        Owner owner = ownerRepository.findByLastName(LAST_NAME);
+
+        assertEquals(LAST_NAME, owner.getLastName());
+
+        verify(ownerRepository).findByLastName(any());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
